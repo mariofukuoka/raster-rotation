2e7aa1cff3fed46b0c3f32f672aed2efff737afa
Mario Fukuoka <mario-fukuoka@outlook.com>
2020-02-08 21:47:47 +0100

    fixed algorithm, can now execute from command line (java RasterRotation <input> <output> <angle>)

--- src/RasterRotation.java
+++ src/RasterRotation.java
@@ -3,12 +3,33 @@ import java.awt.image.*;
 import java.io.File;
 import java.io.IOException;
 
-public class RotationTest {
+public class RasterRotation {
     public static void main(String[] args) throws IOException{
-        BufferedImage img = ImageIO.read(new File("zolta_morda.png"));
+        if (args.length == 0) {
+            System.out.println("USAGE: java RasterRotation <input> <output> <angle>");
+            return;
+        }
+
+        // reading args
+        File input = new File(args[0]);
+        String output_string = args[1];
+        File output = new File(output_string);
+        double angle = Double.parseDouble(args[2]);
+
+        // small code block for getting output file extension from args
+        String outputFileExtension = "";
+        int extensionDotIndex = output_string.lastIndexOf('.');
+        if (extensionDotIndex > 0) {
+            outputFileExtension = output_string.substring(extensionDotIndex + 1);
+        }
+
+        // read input image
+        BufferedImage img = ImageIO.read(input);
 
         int width = img.getWidth();
         int height = img.getHeight();
+
+        // setup for creating output image object
         int[] rgbArray = new int[width*height];
         DataBuffer rgbData = new DataBufferInt(rgbArray, rgbArray.length);
         WritableRaster raster = Raster.createPackedRaster(rgbData, width, height, width,
@@ -16,23 +37,26 @@ public class RotationTest {
         ColorModel colorModel = new DirectColorModel(24, 0xff0000, 0xff00, 0xff);
         BufferedImage rotatedImg = new BufferedImage(colorModel, raster, false, null);
 
+        // setting center of rotation to middle of raster matrix
         double centerX = (double)width/2;
         double centerY = (double)height/2;
-        double angle = 45;
+
+        // looping over dest (rotatedImg) pixels and setting their rgb values
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                //translate origin to center of raster
                 double[] coords = new double[]{i, j};
+                // translating, rotating, then translating back to rotate relative to the image center
                 coords = translate2d(coords, -centerX, -centerY);
                 coords = rotate2d(coords, angle);
                 coords = translate2d(coords, centerX, centerY);
+
                 int newPixelRgb = findFilteredPixelRgb(coords, img);
                 rotatedImg.setRGB(i, j, newPixelRgb);
 
             }
         }
-
-        ImageIO.write(rotatedImg, "png", new File("output.png"));
+        ImageIO.write(rotatedImg, outputFileExtension, output);
+        System.out.println("Image rotated by " + angle + " degrees.");
 
     }
 
@@ -48,30 +72,44 @@ public class RotationTest {
         return new double[]{coords[0] + offsetX, coords[1] + offsetY};
     }
 
+    // "map by area" algorithm
     private static int findFilteredPixelRgb(double[] coords, BufferedImage image) {
         double x = coords[0];
         double y = coords[1];
-        try {
-            int upperLeftPixelRgbInt = image.getRGB((int) Math.floor(x), (int) Math.floor(y));
-            int upperRightPixelRgbInt = image.getRGB((int) Math.ceil(x), (int) Math.floor(y));
-            int lowerLeftPixelRgbInt = image.getRGB((int) Math.floor(x), (int) Math.ceil(y));
-            int lowerRightPixelRgbInt = image.getRGB((int) Math.ceil(x), (int) Math.ceil(y));
+        int floorX = (int) Math.floor(x);
+        int ceilX = (int) Math.ceil(x);
+        int floorY = (int) Math.floor(y);
+        int ceilY = (int) Math.ceil(y);
 
+        try {
+            // 24bit encoded rgb ints of nearest overlapping src pixels
+            int upperLeftPixelRgbInt = image.getRGB(floorX, floorY);
+            int upperRightPixelRgbInt = image.getRGB(ceilX, floorY);
+            int lowerLeftPixelRgbInt = image.getRGB(floorX, ceilY);
+            int lowerRightPixelRgbInt = image.getRGB(ceilX, ceilY);
+
+            // converting to {r,g,b} int value arrays
             int[] upperLeftPixelRgbValues = getRgbValues(upperLeftPixelRgbInt);
             int[] upperRightPixelRgbValues = getRgbValues(upperRightPixelRgbInt);
             int[] lowerLeftPixelRgbValues = getRgbValues(lowerLeftPixelRgbInt);
             int[] lowerRightPixelRgbValues = getRgbValues(lowerRightPixelRgbInt);
 
+            // x and y relative to the upper left overlapping src pixel
+            double relX = x - floorX;
+            double relY = y - floorY;
             int[] desiredPixelRgbValues = new int[3];
+
+            // calculating overlapping-area weighted averages from nearest src pixels for each color channel
             for (int i = 0; i < 3; i++) {
-                desiredPixelRgbValues[i] =  (int) Math.round(((1 - x) * (1 - y) * upperLeftPixelRgbValues[i] +
-                                                              x * (1 - y) * upperRightPixelRgbValues[i] +
-                                                              (1 - x) * y * lowerLeftPixelRgbValues[i] +
-                                                              x * y * lowerRightPixelRgbValues[i]));
+                desiredPixelRgbValues[i] =  (int) Math.round(((1 - relX) * (1 - relY) * upperLeftPixelRgbValues[i] +
+                        relX * (1 - relY) * upperRightPixelRgbValues[i] +
+                        (1 - relX) * relY * lowerLeftPixelRgbValues[i] +
+                        relX * relY * lowerRightPixelRgbValues[i]));
             }
             return getRgbInt(desiredPixelRgbValues);
 
         } catch (ArrayIndexOutOfBoundsException e) {
+            // if this area doesn't map to any pre-rotation src pixel, return a black int rgb
             return 0;
         }
     }
@@ -87,41 +125,4 @@ public class RotationTest {
     private static int[] getRgbValues(int rgbInt) {
         return new int[]{rgbInt>>16,rgbInt>>8&255,rgbInt&255};
     }
-
-
 }
-
-/*
-// ---------pseudocode----------
-// rotation center
-double centerX
-double centerY
-// rotation angle
-double angle
-
-for each pixel in image:
-    Coords translatedCoords = translate(-centerX, -centerY)
-    Coords rotatedCoords = rotate(-angle, translatedCoords)
-    Coords translatedBackCoords = translate(centerX, centerY)
-    int newPixelRgb = findRgbue(translatedBackCoords)
-    pixel.setRgb(newPixelRgb)
-
-
-function findRgbue(x, y, image) -> int:
-    try:
-        int upperLeftPixelRgb = image.getRgb(floor(x), floor(y))
-        int upperRightPixelRgb = image.getRgb(ceil(x), floor(y))
-        int lowerLeftPixelRgb = image.getRgb(floor(x), ceil(y))
-        int lowerRightPixelRgb = image.getRgb(ceil(x), ceil(y))
-
-    catch(outOfIndexException e):
-        return int(0, 0, 0)
-
-    int desiredPixelRgb = ((1 - x) * (1 - y) * upperLeftPixelRgb +
-                           x * (1 - y) * upperRightPixelRgb +
-                           (1 - x) * y * lowerLeftPixelRgb +
-                           x * y * lowerRightPixelRgb)
-
-    return desiredPixelRgb
-
- */
